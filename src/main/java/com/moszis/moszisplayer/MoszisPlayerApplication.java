e0084cca57b51ecb43a5261450061536fd64dcf0
moszis <moszis@yahoo.com>
2019-11-14 14:06:22 -0500

    init

---
+++ src/main/java/com/moszis/moszisplayer/MoszisPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.moszis.moszisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoszisPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoszisPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moszis/moszisplayer/MoszisPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moszis.moszisplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoszisPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
